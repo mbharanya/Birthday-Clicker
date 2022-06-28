dbbecef21b5b31d849274d422cec6809b63ce71a
MBharanya <mbharanya@users.noreply.github.com>
2022-06-28 15:52:15 +0200

    Fix market

--- features/debug.js
+++ features/debug.js
@@ -1,11 +1,16 @@
 const debug = {
-    unlockWax(){
+    unlockWax() {
         game.resources.money = 10000
-        game.candles = 99
-        game.resources.wax = 1
+        game.candles = 100
+        game.resources.wax = 0
+        game.features.hasWaxMarket = true
+        document.getElementById("poshness").style.display = "block"
+        document.getElementById('wax-market').style.display = "block"
     },
     unlockTech() {
         cpu.availableCpus = 500;
+        game.features.hasMarketManipulation = true
+        document.getElementById("posh-market-manipulation").style.display = "block"
         marketManipulator.level = 120
         game.resources.money = 6.6 * 10 ** 6
         game.candles = 10 * 10 ** 6
@@ -23,13 +28,13 @@ const debug = {
         game.resources.unsoldCandles = 10 ** 10
         tech.updatePrices()
     },
-    agasulFight(){
+    agasulFight() {
         this.unlockWeapons()
         tech.quantumLevel = 30
         enemies.activate()
         enemies.updateSpawnAndKill()
     },
-    endGame(){
+    endGame() {
         this.agasulFight()
         sentientCandles.amount = constants.SENTIENT_CANDLE_SOLDIER_UPRISING
     }
